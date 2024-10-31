67821a99c7605f97585799c6c07fa63803b01c4a
Wailed Parsley 36 <71410855+WailedParsley36@users.noreply.github.com>
2024-11-01 00:20:26 +0100

    Update Particles.tsx

--- src/components/Particles.tsx
+++ src/components/Particles.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import React, { useRef, useEffect } from 'react'
+import { useRef, useEffect } from 'react'
 import MousePosition from './utils/mouse-position'
 
 interface ParticlesProps {
